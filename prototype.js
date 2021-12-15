c0937d02aab9d3868e90ea6b5a75a3f51866cbb0
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-12-15 09:37:56 +0530

    Prototype

---
+++ prototype.js
@@ -0,0 +1,15 @@
+let fruit = ["Apple","Mango","Banana"];
+
+ let persondetails = {
+     Name : "praveen",
+     ph_no : 9876544319,
+     Address : "c14,AnnaNagar",
+     display : function (){
+         console.log("Name : "+this.Name+ "Ph.no : "+this.ph_no+"Address : "+this.Address);
+     }
+ }
+
+
+ function empty_function (){
+
+ }
